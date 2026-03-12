323b4e8b964e1ee2f85fd8ec70d1d3a75b6a6b8d
Владислав Карпенко <sdtsbwusbkg@gmail.com>
2026-03-12 11:56:20 +0300

    refactor(prisma): создание единого, переиспользуемого PrismaClient

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,13 +1,27 @@
 import "dotenv/config";
-import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../src/generated/prisma/index.js";
 import { getOrThrowEnv } from "../src/shared/utils/get-or-throw-env.js";
+import isDev from "../src/shared/config/is-dev.js";
 
 const connectionString = getOrThrowEnv("DATABASE_URL");
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
 
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prismaClient: PrismaClient | undefined;
+};
 
-export default prisma;
+const createPrismaClient = () => {
+  const adapter = new PrismaPg({ connectionString });
+
+  return new PrismaClient({
+    adapter,
+  });
+};
+
+const prismaClient = globalForPrisma.prismaClient ?? createPrismaClient();
+
+if (isDev) {
+  globalForPrisma.prismaClient = prismaClient;
+}
+
+export default prismaClient;
